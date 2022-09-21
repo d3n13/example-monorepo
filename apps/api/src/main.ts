32e16d1e5470ffafb255285c3230e5ad48e97c3d
Denis Bilenko <goldenrodcap@gmail.com>
2022-09-21 20:36:21 +0400

    + CRUD

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,10 +3,24 @@ import { Message } from '@example/api-interfaces';
 
 const app = express();
 
-const greeting: Message = { message: 'Welcome to api!' };
-
 app.get('/api', (req, res) => {
-  res.send(greeting);
+  const message: Message = { message: 'Read!' };
+  res.send(message);
+});
+
+app.post('/api', (req, res) => {
+  const message: Message = { message: 'Created!' };
+  res.send(message);
+});
+
+app.put('/api', (req, res) => {
+  const message: Message = { message: 'Updated!' };
+  res.send(message);
+});
+
+app.delete('/api', (req, res) => {
+  const message: Message = { message: 'Deleted!' };
+  res.send(message);
 });
 
 const port = process.env.port || 3333;
--- apps/example-ui/src/app/app.tsx
+++ apps/example-ui/src/app/app.tsx
@@ -1,26 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import React, { ComponentProps, useCallback, useState } from 'react';
 import { Message } from '@example/api-interfaces';
 
 export const App = () => {
   const [m, setMessage] = useState<Message>({ message: '' });
 
-  useEffect(() => {
-    fetch('/api')
+  const makeRequest = useCallback((method: string) => {
+    fetch('/api', { method })
       .then((r) => r.json())
       .then(setMessage);
   }, []);
 
+  const create: ComponentProps<'button'>['onClick'] = useCallback(
+    () => makeRequest('POST'),
+    [makeRequest]
+  );
+  const read: ComponentProps<'button'>['onClick'] = useCallback(
+    () => makeRequest('GET'),
+    [makeRequest]
+  );
+  const update: ComponentProps<'button'>['onClick'] = useCallback(
+    () => makeRequest('PUT'),
+    [makeRequest]
+  );
+  const _delete: ComponentProps<'button'>['onClick'] = useCallback(
+    () => makeRequest('DELETE'),
+    [makeRequest]
+  );
+
   return (
     <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to example-ui!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-          alt="Nx - Smart, Fast and Extensible Build System"
-        />
+      <div>
+        <button onClick={create}>Create</button>
+        <button onClick={read}>Read</button>
+        <button onClick={update}>Update</button>
+        <button onClick={_delete}>Delete</button>
       </div>
-      <div>{m.message}</div>
+      <div>Response fron API: {m.message}</div>
     </>
   );
 };
