513970f1e779d2ac80291fb75a3a7c9ab758936d
Denis Bilenko <goldenrodcap@gmail.com>
2022-09-25 20:55:50 +0400

    improve prepare-docker-files script

--- dockers/prepare-docker-files.sh
+++ dockers/prepare-docker-files.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
-cp -R ../dist/apps/api ../dockers/api/api
-cp -R ../dist/apps/example-ui ../dockers/ui/example-ui
+API_DIST_DIR='../dist/apps/api'
+UI_DIST_DIR='../dist/apps/example-ui'
+API_DOCKER_DIR='../dockers/api/api'
+UI_DOCKER_DIR='../dockers/ui/example-ui'
+
+if [ ! -d "$API_DIST_DIR" ]; then
+  echo 'Api was not built. Try running `yarn build` first';
+  exit 1;
+fi
+
+if [ ! -d "$UI_DIST_DIR" ]; then
+  echo 'UI was not built. Try running `yarn build` first';
+  exit 1;
+fi
+
+[ -d $API_DOCKER_DIR ] && rm -r $API_DOCKER_DIR
+[ -d $UI_DOCKER_DIR ] && rm -r $UI_DOCKER_DIR
+
+cp -R $API_DIST_DIR $API_DOCKER_DIR
+cp -R $UI_DIST_DIR $UI_DOCKER_DIR
 
 cp ../package.json ../dockers/api/package.json
 cp ../yarn.lock ../dockers/api/yarn.lock
